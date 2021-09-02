a8c7a3a035d9c479abc9d45b61c7d59541c8a5db
roku-ahn <ajr2005@naver.com>
2021-09-02 13:23:33 +0900

    Study

---
+++ src/RCC.jsx
@@ -0,0 +1,10 @@
+import React, { Componet } from "react";
+
+class RCC extends Componet {
+  render() {
+      var text = "hihihi"
+    return <div name ='name'>{text}</div>;
+  }
+}
+
+export default RCC;
